e934a0cbae7f9805e945ac751259e9dd746d7c27
Rihanul Islam <rihanthedev@gmail.com>
2023-03-13 21:12:22 +0600

    done objects topic

---
+++ object.js
@@ -0,0 +1,13 @@
+const bio = {
+    name: 'rayhamn',
+    age:14,
+    address: 'Chittagong',
+    phone: [{
+        fax:024654,
+        tel:45487876578,
+        office:6549876546543
+    }]
+}
+console.log(bio.name);
+console.log(bio.phone[0].office);
+console.log(bio['age']);
