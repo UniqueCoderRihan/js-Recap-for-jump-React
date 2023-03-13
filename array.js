6239d5879fc8bdd04670ef4f25aa7c7b837078dc
Rihanul Islam <rihanthedev@gmail.com>
2023-03-13 20:55:23 +0600

    ARRAY

---
+++ array.js
@@ -0,0 +1,6 @@
+const trade = ['computer','autoMobile','Mechanical','electrical','civil Wood'];
+const totalELements = trade.length;
+const bestTrade = trade[1]
+console.log(bestTrade); 
+trade.push('Constraction');
+console.log(trade);
