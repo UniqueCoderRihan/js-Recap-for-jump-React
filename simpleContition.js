306bf087f34e2872f83541d66cb46b8c5d1df45e
Rihanul Islam <rihanthedev@gmail.com>
2023-03-13 20:42:10 +0600

    simpleCondition

---
+++ simpleContition.js
@@ -0,0 +1,8 @@
+// suppos you have Park. but only can entry less than 15 years old man. ticket price 38 taka
+const age = 44;
+if(age<=15){
+    console.log('You Can Go On. ThankYou');
+}
+else{
+    console.log('You Cannot Go There Becuse you are ',age , 'Years old');
+}
