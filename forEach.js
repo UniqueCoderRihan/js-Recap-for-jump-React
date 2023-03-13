718db50e0f975e9cba551c54291405011831b928
Rihanul Islam <rihanthedev@gmail.com>
2023-03-13 23:16:54 +0600

    ForEach goodNight

---
+++ forEach.js
@@ -0,0 +1,4 @@
+let fruits = ["apple", "orange", "cherry", "orange", "cherry", "orange", "cherry", "orange", "cherry", "orange", "cherry", "orange", "cherry", "orange", "cherry"];
+fruits.forEach((n) => {
+    console.log(n);
+});
