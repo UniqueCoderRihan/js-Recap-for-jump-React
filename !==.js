204f2c4a413e69057007a2ad26fe909d7e9999ff
Rihanul Islam <rihanthedev@gmail.com>
2023-03-13 20:48:37 +0600

    !==

---
+++ !==.js
@@ -0,0 +1,8 @@
+const loverBoy = 'raihan';
+const snackName = 'pospa';
+if(loverBoy !== snackName){
+    console.log('itara chon jargoy gorge deh');
+}
+else{
+    console.log('Condition problem ');
+}
