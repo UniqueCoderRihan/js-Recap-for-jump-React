65a265ecf7212ebbec7659bef2063616d51a67a9
Rihanul Islam <rihanthedev@gmail.com>
2023-03-13 20:45:08 +0600

    multipleCondition ==== |||

---
+++ multipleCondition.js
@@ -0,0 +1,8 @@
+const name = 'jabbar';
+const age = 15;
+if(name ==='jabbar' || age === 15){
+    console.log('Condition check done');
+}
+else{
+    console.log('Please Check Your condition or userInput Method.');
+}
