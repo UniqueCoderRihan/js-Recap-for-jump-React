0954f35d2ee0558b0ac055ab79c32961f3814a9f
Rihanul Islam <rihanthedev@gmail.com>
2023-03-13 21:04:45 +0600

    foorLooping

---
+++ loopOnArray.js
@@ -0,0 +1,4 @@
+const trade = ['computer','autoMobile','Mechanical','electrical','civil Wood'];
+for(let i =1; i<=trade.length; i++){
+    console.log('jfsdjfhakfhsdkfjhaksdfhaksdfhaksdfhsdkjfhsdkf');
+}
