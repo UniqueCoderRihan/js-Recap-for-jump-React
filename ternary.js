33175a1960949a8db4f3256abccefca8aab2d18b
Rihanul Islam <rihanthedev@gmail.com>
2023-03-14 11:33:13 +0600

    quiz

--- ternary.js
+++ ternary.js
@@ -1,11 +1,22 @@
-let money = 110;
-let food;
-if(money>100){
-    food='biriany'
-}
-else{
-    food = 'khali caha'
-}
-let food1 =money>100 ?'biriany':'cha biscuits';
-console.log(food1);
-console.log(food);
+// let money = 110;
+// let food;
+// if(money>100){
+//     food='biriany'
+// }
+// else{
+//     food = 'khali caha'
+// }
+// let food1 =money>100 ?'biriany':'cha biscuits';
+// console.log(food1);
+// console.log(food);
+// const name = 'Hero';const age = 34;
+// const person = {name, age}; 
+// console.log(person["name"]);
+// const adventurer = { name: 'Alice',cat: {name: 'Lucy'} };
+// const ratName = adventurer?.rat?.name;
+// console.log(ratName);
+// let person =null;
+// console.log(person ? person : "person is null");
+
+// const obj = {a:1};
+// console.log(Object.keys(obj).length===0);
