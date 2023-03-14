b1841701c81195f77896cbeb3dc6e656a1d4ee03
Rihanul Islam <rihanthedev@gmail.com>
2023-03-14 09:28:09 +0600

    keys-values

---
+++ keys-values.js
@@ -0,0 +1,10 @@
+const students =[{
+    name:'Ahmead Rahi',
+    age:18,
+    address: 'chittagong',
+    study:'Programming Hero'
+}];
+const keys = Object.keys(students[0]);
+const values = Object.values(students[0])
+console.log(keys);
+console.log(values);
