c90da2f6f2d3db63bc96bcf3d4d13405fcae7fd6
Rihanul Islam <rihanthedev@gmail.com>
2023-03-14 10:01:04 +0600

    ternnary

---
+++ ternary.js
@@ -0,0 +1,11 @@
+let money = 110;
+let food;
+if(money>100){
+    food='biriany'
+}
+else{
+    food = 'khali caha'
+}
+let food1 =money>100 ?'biriany':'cha biscuits';
+console.log(food1);
+console.log(food);
