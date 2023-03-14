8c29f713434d5525fa3d93144ab168d97e3926c5
Rihanul Islam <rihanthedev@gmail.com>
2023-03-14 09:24:27 +0600

    destracturing

---
+++ destracturing.js
@@ -0,0 +1,20 @@
+const students =[{
+    name:'Ahmead Rahi',
+    age:18,
+    address: 'chittagong',
+    study:'Programming Hero'
+},
+{
+    name:'Raj Rahi',
+    age:78,
+    address: 'chittagong',
+    study:'Programming Hero'
+},
+{
+    name:'Ahmead NIloy',
+    age:15,
+    address: 'chittagong',
+    study:'Programming Hero'
+}];
+const {name,age,address} =students[0]
+console.log(name,age,address);
